eb213a69018bd60fbd60fdc7e0aa51b957bb8b97
Bambang Witoradyo <bambang.witoradyo@sonoco.com>
2016-11-06 15:42:16 -0500

    add mongodb

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 var express = require('express');
 
-
-
 var app = express();
 
 var port = 5000;
 
 var nav = [{
-    Link:'/Books',
-    Text:'Book'
-},
-    {
-        Link:'/Authors',
-        Text:'Author'
-    }];
+             Link:'/Books',
+             Text:'Book'
+           },
+           {
+             Link:'/Authors',
+             Text:'Author'
+           }];
 
 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
-
-
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -26,6 +23,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
@@ -37,9 +35,9 @@ app.get('/', function (req, res) {
               {
                   Link:'/Authors',
                   Text:'Authors'
-               }]
+              }]
+              });
     });
-});
 
 app.listen(port, function (err) {
     console.log("Server is running on port : " + port);
